2c0027e64d162b821884dc1ccf4c6372ff82ff03
AlexanderShevchenko112 <aleks.shevchenko.2013@gmail.com>
2023-02-20 12:58:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,48 +6,51 @@ const formEl = document.getElementById(`search-form`);
 const loadmoreBtn = document.querySelector('.load-more');
 const gallery = document.querySelector('.gallery');
 
-function fetchImages(name) {
+async function fetchImages(name) {
   const API_KEY = `33764189-9b4498a919581aaa78e0499bc`;
   const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
-  return fetch(BASE_URL);
+  const response = await axios.get(BASE_URL);
+  return response.data;
 }
-
 let query = ``;
 let page = 1;
 
 formEl.addEventListener('submit', getGallery);
 
-function getGallery(event) {
+async function getGallery(event) {
   event.preventDefault();
   const form = event.currentTarget;
   query = form.elements.searchQuery.value.trim();
-
-  fetchImages(query)
-    .then(response => {
-      return response.json();
-    })
-    .then(images => {
-      if (images.hits.length === 0) {
-        resetGallery();
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
+  if (query === ``) {
+    Notiflix.Notify.warning('Please,enter a request');
+    return;
+  }
+  try {
+    const images = await fetchImages(query);
+    console.log(images);
+    if (images.hits.length === 0) {
+      loadmoreBtn.hidden = true;
+      resetGallery();
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      console.log(images);
+      Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images.`);
+      createGalleryMarkup(images.hits);
+      loadmoreBtn.hidden = false;
+      if (images.total === images.totalHits) {
+        loadmoreBtn.hidden = true;
+        Notiflix.Notify.warning(
+          "We're sorry, but you've reached the end of search results."
         );
-      } else {
-        console.log(images);
-        createGalleryMarkup(images.hits);
-        loadmoreBtn.hidden = false;
-        if (images.total === images.totalHits) {
-          loadmoreBtn.hidden = true;
-          Notiflix.Notify.warning(
-            "We're sorry, but you've reached the end of search results."
-          );
-        }
       }
-    })
-    .catch(error => {
-      console.log(error);
-    });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
+
 function createGalleryMarkup(arr) {
   const galleryMarkup = arr
     .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
@@ -55,16 +58,16 @@ function createGalleryMarkup(arr) {
         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
         <div class="info">
           <p class="info-item">
-            <b>Likes:${likes}</b>
+            <b>Likes: ${likes}</b>
           </p>
           <p class="info-item">
-            <b>Views:${views}</b>
+            <b>Views: ${views}</b>
           </p>
           <p class="info-item">
-            <b>Comments:${comments}</b>
+            <b>Comments: ${comments}</b>
           </p>
           <p class="info-item">
-            <b>Downloads:${downloads}</b>
+            <b>Downloads: ${downloads}</b>
           </p>
         </div>
       </div> `;
@@ -80,16 +83,16 @@ function updateGalleryMarkup(arr) {
         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
         <div class="info">
           <p class="info-item">
-            <b>Likes:${likes}</b>
+            <b>Likes: ${likes}</b>
           </p>
           <p class="info-item">
-            <b>Views:${views}</b>
+            <b>Views: ${views}</b>
           </p>
           <p class="info-item">
-            <b>Comments:${comments}</b>
+            <b>Comments: ${comments}</b>
           </p>
           <p class="info-item">
-            <b>Downloads:${downloads}</b>
+            <b>Downloads: ${downloads}</b>
           </p>
         </div>
       </div> `;
@@ -103,16 +106,21 @@ function resetGallery() {
 }
 
 loadmoreBtn.addEventListener(`click`, loadMore);
-function loadMore() {
+async function loadMore() {
   page++;
-  fetchImages(query)
-    .then(response => {
-      return response.json();
-    })
-    .then(images => {
-      updateGalleryMarkup(images.hits);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  try {
+    const newImages = await fetchImages(query);
+    updateGalleryMarkup(newImages.hits);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
